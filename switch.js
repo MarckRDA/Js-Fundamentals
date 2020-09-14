00e2b8100d7867dadd0dacb2bfa4a084b94d045c
MarckRDA <marcos.edo132@gmail.com>
2020-09-14 15:19:35 -0300

    Added switch-case structure

---
+++ switch.js
@@ -0,0 +1,29 @@
+
+document.write('O que deseja comer?')
+document.write('1 - Salada')
+document.write('2 - Bolo')
+document.write('3 - Hamburguer')
+document.write('4 - Sanduiche')
+document.write('5 - Macarronada')
+
+var optionUser = prompt('Digite a opção desejada:')
+
+switch (parseInt(optionUser){
+	case 1:
+		document.write('Você escolheu Salada!')
+		break
+	case 2:
+		document.write('Você escolheu Bolo!')
+		break
+	case 3:
+		document.write('Você escolheu Hamburguer!')
+		break
+	case 4:
+		document.write('Você escolheu Sanduiche!')
+		break
+	case 5:
+		document.write('Você escolheu Macarronada!')
+		break
+	default:
+		break
+}
