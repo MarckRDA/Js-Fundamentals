7a7c30119b2c9213ab893c781c52a99887f484bc
MarckRDA <marcos.edo132@gmail.com>
2020-09-14 12:21:34 -0300

    First commit

---
+++ variaveis.js
@@ -0,0 +1,21 @@
+//String
+var titulo_curso = "Curso de JavaScript"
+
+//numbers
+var intNumber = -7
+var floatNumber = 123.43
+
+//boolean
+var amITrue = true
+var amIFalse = false
+
+//Open a dialog on the browser
+alert(amITrue)
+document.write(titulo_curso)
+console.log(intNumber)
+
+//null
+var nullable = null
+
+//undefined
+var undefinedValue
